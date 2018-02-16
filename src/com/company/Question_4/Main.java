5c8cec7b8a5050e94873f9ae06824e5245cd3420
Elijah Smith <elijahsmith@Interapts-MacBook-Pro-71.local>
2018-02-16 15:49:58 -0500

    Finished Q5

--- src/com/company/Question_4/Main.java
+++ src/com/company/Question_4/Main.java
@@ -1,8 +1,15 @@
 package com.company.Question_4;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
+    public Scanner input = new Scanner(System.in);
+    int userInput;
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.teleportationCirlce();
+    }
         /*
         1. Create a new method to take user input of a number between 1 and 100
         2. Create an if statement that will take this number and if the number is between 1 and/or equal to 45 display,
@@ -10,6 +17,39 @@ public class Main {
         teleported to the beach".  If the number is between 91 and/or equal to 100 display, "Well, I hope you like it hot,
         you have been teleported to a volcano."
          */
-    }
 
+    public void teleportationCirlce() {
+        try {
+
+            System.out.println("Please enter a number between 1 and 100");
+            input.nextInt();
+
+            if (userInput > 1) {
+            } else if (userInput <= 45) {
+                System.out.println("You have been teleported to a swamp.");
+            } else if (userInput <= 90) {
+                System.out.println("Lucky you, you've been teleported to a beach!");
+            } else if (userInput <= 100) {
+                System.out.println("Well, I hope you like it hot, you have been teleported to a volcano.");
+
+            }
+
+//            if (1 <= userInput && 45 >= userInput) {
+//                System.out.println("You have been teleported to a swamp.");
+//            } else if (userInput >= 46 && userInput <= 90) {
+//                System.out.println("Lucky you, you've been teleported to a beach!");
+//            } else if (userInput > 91 && userInput <= 100) {
+//                System.out.println("Well, I hope you like it hot, you have been teleported to a volcano.");
+//            //}else if (userInput < 1 || userInput > 100) {
+//              //  System.out.println("That is not a valid option");
+//                teleportationCirlce();
+        } catch (InputMismatchException ime) {
+            input.nextInt();
+            System.out.println();
+            teleportationCirlce();
+        }
+
+
+    }
 }
+
--- src/com/company/Question_5/Main.java
+++ src/com/company/Question_5/Main.java
@@ -1,8 +1,13 @@
 package com.company.Question_5;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.attackNumber();
          /*
          1. Create a new method in this class.  Make sure to instantiate the main to break out of the static context.
          2. Create a switch statement in this new method that has 4 cases and a default.  This switch should take user input of a string .
@@ -12,5 +17,34 @@ public class Main {
 
 
     }
+    protected void attackNumber() {
+        System.out.println("Choose an attack!Select a number");
+        System.out.println("1.Punch \n2.Kick \n3.HeadButt \n4.Run");
+        switch (input.nextLine()) {
+            case "1":
+                System.out.println("PUNCH!");
+                attackNumber();
+                break;
+            case "2":
+                System.out.println("KICK!");
+                attackNumber();
+                break;
+            case "3":
+                System.out.println("HEADBUTT!");
+                attackNumber();
+                break;
+            case "4":
+                System.out.println("COWARD!");
+                System.exit(0);
+                break;
+                default:
+                    System.out.println("Please make a selection");
+                    attackNumber();
+                    break;
+
+
+        }
+    }
+
 
 }
