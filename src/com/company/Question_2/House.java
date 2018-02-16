8dba2af38f0ae253e0b4abc22a265fd79f5c7b29
Elijah Smith <elijahsmith@Interapts-MacBook-Pro-71.local>
2018-02-16 14:50:57 -0500

    Finished Question 2

---
+++ src/com/company/Question_2/House.java
@@ -0,0 +1,27 @@
+package com.company.Question_2;
+
+public class House {
+    private String color;
+    private int age;
+
+    public House(String color, int age) {
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
